44639e04d564bce95eed9a31e6a24854092ceeab
mokuest12 <45183498+mokuest12@users.noreply.github.com>
2022-10-15 05:48:08 +0800

    Add files via upload

---
+++ field.java
@@ -0,0 +1,10 @@
+public class field {
+		public static void main(String[] args){
+		int pageCount;
+		double price;
+		String 111;
+		'field-1': <String>;
+		System.out.println(pageCount); 
+		System.out.println(price);
+		}
+}
